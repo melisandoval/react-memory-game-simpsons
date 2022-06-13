49846b0a86aabcfc7e696e08982f5e8afa2f0a05
melisand <melisandoval@live.com.ar>
2022-06-13 16:13:29 +0200

    attribute added to anchor tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,18 @@ export default function Footer() {
       <footer>
         <p>
           Coded by{" "}
-          <a href="https://github.com/melisandoval" target="_blank">
+          <a
+            href="https://github.com/melisandoval"
+            target="_blank"
+            rel="noreferrer"
+          >
             Melisa Sandoval
           </a>
           . Images sources:{" "}
           <a
             href="https://en.wikipedia.org/wiki/List_of_The_Simpsons_characters"
             target="_blank"
+            rel="noreferrer"
           >
             Wikipedia
           </a>{" "}
@@ -20,6 +25,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Homer_Simpson_2006.png"
             target="_blank"
+            rel="noreferrer"
           >
             Homer
           </a>
@@ -27,6 +33,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Marge_Simpson.png"
             target="_blank"
+            rel="noreferrer"
           >
             Marge
           </a>
@@ -34,6 +41,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Bart_Simpson_200px.png"
             target="_blank"
+            rel="noreferrer"
           >
             Bart
           </a>
@@ -41,6 +49,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Lisa_Simpson.png"
             target="_blank"
+            rel="noreferrer"
           >
             Lisa
           </a>
@@ -48,6 +57,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Chief_Wiggum.png"
             target="_blank"
+            rel="noreferrer"
           >
             Chief
           </a>
@@ -55,6 +65,7 @@ export default function Footer() {
           <a
             href="https://en.wikipedia.org/w/index.php?curid=15969054"
             target="_blank"
+            rel="noreferrer"
           >
             Krusty
           </a>
@@ -62,6 +73,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Ned_Flanders.png"
             target="_blank"
+            rel="noreferrer"
           >
             Flanders
           </a>{" "}
@@ -69,6 +81,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:Seymour_Skinner.png"
             target="_blank"
+            rel="noreferrer"
           >
             Skinner
           </a>
@@ -76,6 +89,7 @@ export default function Footer() {
           <a
             href="//en.wikipedia.org/wiki/File:SantasLittleHelper.png"
             target="_blank"
+            rel="noreferrer"
           >
             Santa's Helper
           </a>
@@ -83,6 +97,7 @@ export default function Footer() {
           <a
             href="https://wallpaperforu.com/wallpaper-clouds-figure-background-simpsons-art-begin/"
             target="_blank"
+            rel="noreferrer"
           >
             wallpaperforu.com
           </a>
@@ -90,6 +105,7 @@ export default function Footer() {
           <a
             href="https://www.popsugar.co.uk/tech/photo-gallery/47382857/image/47382834/Simpsons-Zoom-Background?utm_medium=redirect&utm_campaign=US:ES&utm_source=localhost:3000"
             target="_blank"
+            rel="noreferrer"
           >
             popsugar.com
           </a>
@@ -97,11 +113,16 @@ export default function Footer() {
           <a
             href="https://pngset.com/images/se-compara-tu-cv-con-el-de-homero-simpson-schmitman-sunglasses-accessories-accessory-face-transparent-png-2229784.png"
             target="_blank"
+            rel="noreferrer"
           >
             pngset.com
           </a>{" "}
           and{" "}
-          <a href="https://www.pngwing.com/es/free-png-bulfi" target="_blank">
+          <a
+            href="https://www.pngwing.com/es/free-png-bulfi"
+            target="_blank"
+            rel="noreferrer"
+          >
             pngwing.com
           </a>
           .
